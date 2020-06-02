6ca3a343379ae484e2f5e4fb08f2bfd47eb7a4b1
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-02 13:55:17 +0530

    Understanding and using state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 class App extends Component {
+  state = {
+    person: [
+      { name: "Shahid", age: 23 },
+      { name: "Luffy", age: 18 },
+      { name: "Zoro", age: 30 },
+    ],
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi, I am react app</h1>
         <p>Hola</p>
-        <Person name="Shahid Dhariwala" age="23" /> 
-        <Person name=" Luffy" age ="18"> <h3> Strawhat : Captain </h3></Person>
-        <Person name="Zoro" age="30">  <h4> Strawhat : Swordsman/Vice-Captain </h4> </Person>
+        <Person
+          name={this.state.person[0].name}
+          age={this.state.person[0].age}
+        />
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>
+          <h3> Strawhats : Captain </h3>
+        </Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}>
+          <h4> Strawhats : Vice-Captain / Swordsman </h4>
+        </Person>
       </div>
-      // <p>Hola</p> not allowed , it is recommended to have one root elements inside it 
+      // <p>Hola</p> not allowed , it is recommended to have one root elements inside it
       // other elements to be wrapped ( * before React 16);
     );
 
