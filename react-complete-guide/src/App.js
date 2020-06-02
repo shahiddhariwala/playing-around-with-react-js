2f0907e274a7629f50d66328c0fbebd660b4c6ac
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-02 12:36:02 +0530

    Understanding JSX

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,9 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi, I am react app</h1>
+        <h1>Hi, I am react app</h1>
       </div>
     );
+
+    // return React.createElement('div',{className: 'App'},React.createElement('h1',null,' I am a react App'));
   }
 }
 
