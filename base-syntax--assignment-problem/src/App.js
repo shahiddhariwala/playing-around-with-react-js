4d402fa3ea4b490fd5b46b8514c86da98a6fc08a
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-02 21:05:51 +0530

    Done : Add state to the App component (=> the username) and pass the username to the UserOutput component

--- base-syntax--assignment-problem/src/App.js
+++ base-syntax--assignment-problem/src/App.js
@@ -5,12 +5,17 @@ import UserInput from './User/UserInput';
 import UserOutput from './User/UserOutput';
 
 class App extends Component {
+  state = {
+    users:[
+     'Shahid','Zain','Luffy'
+    ]
+  }
   render() {
     return (
       <div className="App">
        <UserInput></UserInput>
-       <UserOutput userName="Shahid"></UserOutput>
-       <UserOutput userName="Zain"></UserOutput>
+       <UserOutput userName={this.state.users[0]}></UserOutput>
+       <UserOutput userName={this.state.users[2]}></UserOutput>
       </div>
     );
   }
