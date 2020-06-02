5f5a3a54dbdcae26c267e4acf3dcd9775a4f162d
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-02 22:48:20 +0530

    Done : Final Touches

--- base-syntax--assignment-problem/src/App.js
+++ base-syntax--assignment-problem/src/App.js
@@ -14,11 +14,13 @@ class App extends Component {
   };
   render() {
     const style = {
- 
+
       position: "fixed",
       left: "0",
-      bottom: "-9999",
+      bottom: "0",
       width: '100%',
+      height:'30px',
+      paddingTop: '10px',
       background: 'linear-gradient(to right, #4ca2cd,#67b26f)',
       color: 'white',
       textAlign: 'center',
