3265f327603a814d8c15b4cb6adb1a60d89c4b84
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-02 13:04:25 +0530

    JSX and Component creation info

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const person = ()=>
-{
-    return <p> I am Shahid Dhariwala </p>;
-}
-
+const person = () => {
+  return <p> I am Shahid Dhariwala </p>;
+};
 export default person;
+/*
+JSX is NOT HTML but it looks a lot like it. Differences can be seen when looking closely though (for example className in JSX vs class in "normal HTML"). JSX is just syntactic sugar for JavaScript, allowing you to write HTMLish code instead of nested React.createElement(...) calls.
+
+When creating components, you have the choice between two different ways:
+
+Functional components (also referred to as "presentational", "dumb" or "stateless" components) => const cmp = () => { return <div>some JSX</div> } (using ES6 arrow functions as shown here is recommended but optional)
+class-based components (also referred to as "containers", "smart" or "stateful" components) => class Cmp extends Component { render () { return <div>some JSX</div> } } 
+*/
