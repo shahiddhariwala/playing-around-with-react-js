181e88b650a9120806a964ea01121564a5cfc47d
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-02 12:57:55 +0530

    Creating functional Components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I am react app</h1>
         <p>Hola</p>
+        <Person />
       </div>
       // <p>Hola</p> not allowed , it is recommended to have one root elements inside it 
       // other elements to be wrapped ( * before React 16);
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const person = ()=>
+{
+    return <p> I am Shahid Dhariwala </p>;
+}
+
+export default person;
