bdf5772a02dc6e6d729b10b444343cf1e8674e18
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-02 22:37:09 +0530

    Done : Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets

--- base-syntax--assignment-problem/src/App.js
+++ base-syntax--assignment-problem/src/App.js
@@ -13,11 +13,28 @@ class App extends Component {
     this.setState({ users: [event.target.value, "Zain", "Luffy"] });
   };
   render() {
+    const style = {
+ 
+      position: "fixed",
+      left: "0",
+      bottom: "-9999",
+      width: '100%',
+      background: 'linear-gradient(to right, #4ca2cd,#67b26f)',
+      color: 'white',
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: 'large',
+    };
+
     return (
       <div className="App">
-        <UserInput changed={this.inputChangeHandler} name={this.state.users[0]}></UserInput>
+        <UserInput 
+          changed={this.inputChangeHandler}
+          name={this.state.users[0]}
+        ></UserInput>
         <UserOutput userName={this.state.users[0]}></UserOutput>
         <UserOutput userName={this.state.users[2]}></UserOutput>
+        <p style={style}>&copy; Shahid Dhariwala</p>
       </div>
     );
   }
--- base-syntax--assignment-problem/src/User/UserOutput.js
+++ base-syntax--assignment-problem/src/User/UserOutput.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 const userOutput = (props) => {
   return (
-    <div class="box">
+    <div className="box">
       <p>I am {props.userName}</p>
       <p>This paragraph 2 </p>
     </div>
