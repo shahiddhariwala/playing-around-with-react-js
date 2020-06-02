9e8f784f9a148e13665080d256b6aa3de1ef48ac
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-02 21:10:17 +0530

    Add a method to manipulate the state (=> an event-handler method)

--- base-syntax--assignment-problem/src/App.js
+++ base-syntax--assignment-problem/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import UserInput from './User/UserInput';
-import UserOutput from './User/UserOutput';
+import UserInput from "./User/UserInput";
+import UserOutput from "./User/UserOutput";
 
 class App extends Component {
   state = {
-    users:[
-     'Shahid','Zain','Luffy'
-    ]
-  }
+    users: ["Shahid", "Zain", "Luffy"],
+  };
+
+  eventHandler = (event) => {
+    this.setState({ users: [event.target.value, "Zain", "Luffy"] });
+  };
   render() {
     return (
       <div className="App">
-       <UserInput></UserInput>
-       <UserOutput userName={this.state.users[0]}></UserOutput>
-       <UserOutput userName={this.state.users[2]}></UserOutput>
+        <UserInput></UserInput>
+        <UserOutput userName={this.state.users[0]}></UserOutput>
+        <UserOutput userName={this.state.users[2]}></UserOutput>
       </div>
     );
   }
