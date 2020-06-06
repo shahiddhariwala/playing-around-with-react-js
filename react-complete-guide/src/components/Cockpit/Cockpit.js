b7228723a5ef8b1632002a4d85c3e6f6a60bbef3
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-06 13:19:36 +0530

    Using useEffect a react hook in functional component

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,31 +1,40 @@
-import React from 'react';
-import classes from './Cockpit.css'
-const cockpit = (props) =>
-{
-    const assignedClasses = [];
-    let btnClass ='';
-    if(props.showPersons)
-    {
-        btnClass = classes.Red;
-    }
-    if(props.person.length <=2)
-    {
-      assignedClasses.push(classes.red);
-    }
-    if(props.person.length <=1)
-    {
-      assignedClasses.push(classes.bold);
-    }
-    
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is a working app</p>
-            <button  className={btnClass} onClick={props.clicked}>
-            Hide / Unhide
-            </button>
-        </div>
-    );
-}
+import React, { useEffect } from "react";
+import classes from "./Cockpit.css";
+const cockpit = (props) => {
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect");
+    /*
+    This is not a lifecycle hook,
+    this is a React hook
+    So useEffect runs here, it runs for every update and
+    this means we can already use it for
+    all the things we would have done in componentDidUupdate and indeed that is OK.
+     If you need to send
+an HTTP request or anything like that in here, 
+you can do that. It also obviously runs when the
+component is created,  */
+  });
+  const assignedClasses = [];
+  let btnClass = "";
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+  if (props.person.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
+  if (props.person.length <= 1) {
+    assignedClasses.push(classes.bold);
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(" ")}>This is a working app</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Hide / Unhide
+      </button>
+    </div>
+  );
+};
 
 export default cockpit;
