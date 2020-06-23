d44ab5c01331957b50204b0b17668a0f80ceea58
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-23 16:18:08 +0530

    Dispaching Action

--- redux--01-start/src/redux_basics.js
+++ redux--01-start/src/redux_basics.js
@@ -25,12 +25,35 @@ const initialState = {
 };
 
 //Reducer
-const rootReducer = (state = initialState, action) => {return state};
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INC_COUNTER":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+      break;
+    case "ADD_COUNTER":
+      return {
+        ...state,
+        counter: state.counter + action.value,
+      };
+      break;
+    default:
+      return state;
+      break;
+  }
+};
 
 //Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
 //Dispatching Action
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+// compulsory to have type property
+console.log(store.getState());
+
 
 //Subscription
